fd7845be46568a3a25b89148101a9a4d330fea20
Valentin Funk <funk.valentin@gmail.com>
2016-02-23 12:11:39 +0100

    initial

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public/app'));
+app.use(express.static('public'));
+app.listen(80, function() {
+  console.log("Static Content server gestartet");
+});
