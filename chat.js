cda2132bf79095b8a0465740da07d36d057036b8
Valentin Funk <funk.valentin@gmail.com>
2016-02-23 18:07:43 +0100

    Chat Implementierung

---
+++ chat.js
@@ -0,0 +1,46 @@
+var Server = require('socket.io');
+
+var io = new Server();
+
+var users = {};
+
+function getUsernames( ) {
+  var usernames = [];
+  for (var key in users) {
+    usernames.push(users[key]);
+  }
+  return usernames;
+}
+
+io.on('connection', function(socket) {
+  socket.emit('channelinfo', getUsernames());
+
+  socket.on('setname', function(username) {
+    users[socket.id] = username;
+    io.emit('join', username);
+  });
+
+  socket.on('disconnect', function() {
+    io.emit('leave', users[socket.id]);
+    delete users[socket.id];
+  })
+
+  socket.on('message', function(message) {
+    var username = users[socket.id];
+    if (!username) {
+      console.log("Benutzer hat versucht, ohne Namen eine Nachricht abzuschicken");
+      return;
+    }
+
+    io.emit('message', {
+      message: message,
+      username: username
+    });
+  });
+});
+
+module.exports = function(app) {
+  io.attach(app);
+
+  console.log("Chat Server (socket.io) gestartet")
+}
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express');
+var express = require('express'),
+    http = require('http');
+
+var chat = require('./chat');
+
 var app = express();
 
 app.use(express.static('public/app'));
 app.use(express.static('public'));
-app.listen(80, function() {
+
+var server = http.createServer(app);
+server.listen(80, function() {
   console.log("Static Content Server gestartet.");
 });
+
+chat(server);
